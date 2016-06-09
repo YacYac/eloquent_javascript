a6846fccba845f82f073668a4bfea00d2850bd9d
Blair Yacishyn <b.yacishyn@gmail.com>
2016-06-09 13:11:10 -0600

    added countLetters function to beanCounter.js

--- beanCounter.js
+++ beanCounter.js
@@ -9,4 +9,16 @@ function countBs(string) {
   return numBs;
 }
 
+function countLetters(stringToSearch,charToFind) {
+  var charsFound = 0;
+  for (var i = 0; i < stringToSearch.length; i++) {
+    if (stringToSearch.charAt(i) === charToFind) {
+      charsFound += 1;
+    }
+  }
+
+  return charsFound;
+}
+
 console.log(countBs("The Berenstein Bears are Bringing It Home."));
+console.log(countLetters("Help me help you","e"));
