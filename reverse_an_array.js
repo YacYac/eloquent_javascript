9e7f7a069752bf0c743bd12fa8ac57a390c6bac9
Blair Yacishyn <b.yacishyn@gmail.com>
2016-06-25 11:20:38 -0600

    added reverseArrayInPlace feature

--- reverse_an_array.js
+++ reverse_an_array.js
@@ -1,16 +1,22 @@
 function reverseArray(array) {
   var newArray = [];
-      
+
   for (var i = array.length; i > 0; i--) {
     newArray.push(i);
   }
-  
+
   return newArray;
 }
 
 function reverseArrayInPlace(array) {
+  var temp;
+  for (var i = 0; i < array.length/2; i++) {
+    temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
   // reverse array without making a new array
   return array;
 }
-    
-console.log(reverseArray([1,2,3,4,5]));
+
+console.log(reverseArrayInPlace(reverseArray([1,2,3,4,5,6,7,8])));
