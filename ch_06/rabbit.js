cc2930cad21d6786f8eecb1c875cbe0b4f9e64cd
Blair Yacishyn <b.yacishyn@gmail.com>
2016-06-30 14:36:27 -0600

    added apply and call methods

--- ch_06/rabbit.js
+++ ch_06/rabbit.js
@@ -1,6 +1,6 @@
 var rabbit = {};
 function speak(line) {
-  console.log("The " + this.type + " says '" + line + "'");
+  console.log("The " + this.type + " rabbit says '" + line + "'");
 };
 var whiteRabbit = {type: "white", speak: speak};
 var fatRabbit = {type: "fat", speak: speak};
@@ -8,3 +8,6 @@ var fatRabbit = {type: "fat", speak: speak};
 whiteRabbit.speak("Oh my ears and whiskers, " +
                   "how late it's getting!");
 fatRabbit.speak("I sure could use a carrot right now.")
+
+speak.apply(fatRabbit, ["Burp!"]);
+speak.call({type: "old"}, "Oh my.");
