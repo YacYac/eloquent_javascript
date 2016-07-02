cb3d3e804b361450377f7a84321a949800293112
Blair Yacishyn <b.yacishyn@gmail.com>
2016-07-01 22:31:49 -0600

    added StretchCell

--- ch_06/table.js
+++ ch_06/table.js
@@ -124,6 +124,29 @@ RTextCell.prototype.draw = function (width, height) {
   return result;
 };
 
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+
+StretchCell.prototype.minWidth = function () {
+  return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function () {
+  return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function (width, height) {
+  return this.inner.draw(width, height);
+};
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log();
+console.log(sc.minWidth());
+console.log(sc.minHeight());
+console.log(sc.draw(3, 2));
+
+
 fs.readFile('mountains.json',function (err,data) {
   var mountains = JSON.parse(data);
 
