96b0f238d4eb4f1f711a669d491d0227b8b43a49
Blair Yacishyn <b.yacishyn@gmail.com>
2016-07-16 05:05:09 -0600

    added several functions and made the world printable

--- ch_07/electronic_life.js
+++ ch_07/electronic_life.js
@@ -35,7 +35,76 @@ Grid.prototype.set = function (vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 };
 
-var grid = new Grid(5,5);
-console.log(grid.get(new Vector(1, 1)));
-grid.set(new Vector(1, 1), "X");
-console.log(grid.get(new Vector(1, 1)));
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = "n ne e se s sw w nw".split(" "); //makes an array
+
+function BouncingCritter() {
+  this.directon = randomElement(directionNames);
+}
+
+BouncingCritter.prototype.act = function (view) {
+  if (view.look(this.directon) != " ") {
+    this.directon = view.find(" ") || "s";
+  }
+  return {type: "move", directon: this.direction};
+};
+
+function elementFromChar(legend, ch) {
+  if (ch == " ") {
+    return null;
+  }
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function World(map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function (line, y) {
+    for (var x = 0; x < line.length; x++) {
+      grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+    }
+  });
+}
+
+function charFromElement(element) {
+  if (element == null) {
+    return " ";
+  } else {
+    return element.originChar;
+  }
+}
+
+World.prototype.toString = function () {
+  var output = "";
+  for (var y = 0; y < this.grid.height; y++) {
+    for (var x = 0; x < this.grid.width; x++) {
+      var element = this.grid.get(new Vector(x, y));
+      output += charFromElement(element);
+    }
+    output += "\n";
+  }
+  return output;
+};
+
+function Wall() {}
+
+var world = new World(plan, {"#": Wall, "o": BouncingCritter});
+console.log(world.toString());
